b2836fd420a53334bd60b95f7e85733a983d8239
cbarrios <lalobarrios16@yahoo.com>
2022-09-18 20:07:19 -0400

    master: (3) picture in picture done

--- app/src/main/java/com/example/pictureinpicture/MainActivity.kt
+++ app/src/main/java/com/example/pictureinpicture/MainActivity.kt
@@ -1,16 +1,101 @@
 package com.example.pictureinpicture
 
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Rect
+import android.graphics.drawable.Icon
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
+import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toAndroidRect
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.pictureinpicture.ui.theme.PictureInPictureTheme
 
 class MainActivity : ComponentActivity() {
+
+    class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            println("Clicked on pip action")
+        }
+    }
+
+    private val isPipSupported by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            packageManager.hasSystemFeature(
+                PackageManager.FEATURE_PICTURE_IN_PICTURE
+            )
+        } else {
+            false
+        }
+    }
+
+    private var videoViewBounds = Rect()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PictureInPictureTheme {
+                AndroidView(factory = {
+                    VideoView(it, null).apply {
+                        setVideoURI(Uri.parse("android.resource://$packageName/${R.raw.sample}"))
+                        start()
+                    }
+                }, modifier = Modifier
+                    .fillMaxWidth()
+                    .onGloballyPositioned {
+                        videoViewBounds = it
+                            .boundsInWindow()
+                            .toAndroidRect()
+                    })
+            }
+        }
+    }
 
+    private fun updatePipParams(): PictureInPictureParams? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            PictureInPictureParams.Builder().setSourceRectHint(videoViewBounds)
+                .setAspectRatio(Rational(16, 9)).setActions(
+                    listOf(
+                        RemoteAction(
+                            Icon.createWithResource(
+                                applicationContext, R.drawable.ic_baseline_bedroom_baby_24
+                            ), "Bedroom Baby", "Bedroom Baby", PendingIntent.getBroadcast(
+                                applicationContext,
+                                0,
+                                Intent(applicationContext, MyReceiver::class.java),
+                                PendingIntent.FLAG_IMMUTABLE
+                            )
+                        )
+                    )
+                ).build()
+        } else {
+            null
+        }
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if (!isPipSupported) {
+            return
+        }
+        updatePipParams()?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                enterPictureInPictureMode(
+                    it
+                )
             }
         }
     }
